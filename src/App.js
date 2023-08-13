d72412ac4953acb46440ee36eee9ad679f8436cc
Matthaeus <matthauswolff@gmail.com>
2023-08-13 14:33:13 -0700

    fixes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       let index = 1;
 
       while (mounted) {
-        const imagePath = `/assets/${index}.png`;
+        const imagePath = `./assets/${index}.png`;
 
         try {
           const response = await fetch(imagePath);
